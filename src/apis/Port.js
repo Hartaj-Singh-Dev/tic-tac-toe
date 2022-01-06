064da42c9b7bb9703aa1cbfae8a1aa8c3565219e
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2022-01-06 20:34:04 +0530

    Bug Testing

--- src/apis/Port.js
+++ src/apis/Port.js
@@ -1,9 +1,7 @@
 import io from "socket.io-client";
-// import dotenv from "dotenv";
 
-// dotenv.config({path:"../../config.env"})
-
-const port = process.env.REACT_APP_BACKEND_LINK//need to change before deployment to production
+const port = process.env.REACT_APP_BACKEND_LINK
 
 const socket = io(port);
+
 export default socket;
